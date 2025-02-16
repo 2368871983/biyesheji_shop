04bf49814bb7b2deccd8286184d61a950ae154a7
2368871983 <2368871983@qq.com>
2025-02-16 22:00:15 +0800

    首页基本渲染

--- src/views/layout/HomePage.vue
+++ src/views/layout/HomePage.vue
@@ -1,4 +1,34 @@
-<script setup></script>
+<script setup>
+import { onMounted } from 'vue'
+import { getHomeData } from '@/api/home'
+import { ref } from 'vue'
+import Goods_item from '@/components/Goods_item.vue'
+
+const bannerList = ref([])
+const navList = ref([])
+const goodsList = ref([])
+const isLoading = ref(false) // 加载状态
+const page = ref(0) // 当前页码
+onMounted(async () => {
+  await loadData()
+})
+const loadData = async () => {
+  isLoading.value = true
+  try {
+    const {
+      data: { pageData },
+    } = await getHomeData(page.value)
+    bannerList.value = pageData.items[1].data
+    navList.value = pageData.items[3].data
+    goodsList.value = [...goodsList.value, ...pageData.items[6].data]
+    page.value++
+  } catch (error) {
+    console.error('加载数据失败:', error)
+  } finally {
+    isLoading.value = false
+  }
+}
+</script>
 
 <template>
   <van-nav-bar title="购物商城" />
@@ -6,15 +36,15 @@
   <van-search @click="$router.push('/search')" shape="round" placeholder="请输入搜索关键词" />
 
   <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
-    <van-swipe-item><img src="@/assets/banner1.jpg" alt="" /></van-swipe-item>
-    <van-swipe-item><img src="@/assets/banner2.jpg" alt="" /></van-swipe-item>
-    <van-swipe-item><img src="@/assets/banner3.jpg" alt="" /></van-swipe-item>
+    <van-swipe-item v-for="item in bannerList" :key="item.imgUrl"
+      ><img :src="item.imgUrl" alt=""
+    /></van-swipe-item>
   </van-swipe>
   <van-grid column-num="5" icon-size="40">
     <van-grid-item
-      v-for="item in 10"
-      :key="item"
-      icon="@/assets/nav1.png"
+      v-for="item in navList"
+      :key="item.imgUrl"
+      :icon="item.imgUrl"
       text="新品首发"
       @click="$router.push('/category')"
     />
@@ -28,8 +58,10 @@
     <p class="guess-title">—— 猜你喜欢 ——</p>
 
     <div class="goods-list">
-      <GoodsItem v-for="item in 10" :key="item"></GoodsItem>
+      <Goods_item v-for="item in goodsList" :key="item.goods_id" :item="item"></Goods_item>
     </div>
+    <!-- 加载提示 -->
+    <div v-if="isLoading">加载中...</div>
   </div>
 </template>
 
@@ -67,6 +99,7 @@
 
 // 商品样式
 .goods-list {
-  background-color: #f6f6f6;
+  background: linear-gradient(180deg, #efb4b0, #fff3f3);
+  margin-bottom: 40px;
 }
 </style>
